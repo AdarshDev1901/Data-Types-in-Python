ab0c1c2b6b3ccc1f74fec6fef711db306f8e0619
Adarsh <adarshdevadakar1901@gmail.com>
2026-09-02 21:44:52 +0530

    Added Practice Q2

---
+++ Codes/All Practice Q & A/2.Practice Q2.py
@@ -0,0 +1,10 @@
+"""
+Q2: Take a number as input. Print whether it is even or odd using the %
+operator and a comparison operator.
+"""
+
+num = int(input("Enter number = "))
+if num % 2 == 0:
+    print("Number is Even")
+else:
+    print("Number is Odd")

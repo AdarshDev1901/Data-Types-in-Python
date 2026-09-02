1d3cfca4cf40d3c06b5baff8d3b9dbdfb8a8c192
Adarsh <adarshdevadakar1901@gmail.com>
2026-09-02 19:33:29 +0530

    2. Escape Sequence and Operators Examples added

---
+++ Codes/2. Escape Sequence and Operators/1. escape_sequence.py
@@ -0,0 +1,17 @@
+"""
+\n - New Line
+\t - Tab Space
+\\ - Backslash
+\" - Double quotes
+\' - Single quotes
+"""
+
+print("He\nllo\nWorl\nd")
+print("Hello\tWorld")
+
+# A\\\Adarsh
+# print("A\\\\\\Adarsh")
+
+# My name is "Anirudh" Devadakar
+# print('My name is "Adarsh" Devadakar')
+# print("My name is 'Adarsh' Devadakar")
